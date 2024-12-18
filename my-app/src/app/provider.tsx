8743f6622019bd3d9b4f89c7346b22f6278d4dcf
Man Qin <manqin9@gmail.com>
2024-12-18 10:58:01 +0800

    revise1

--- my-app/src/app/provider.tsx
+++ my-app/src/app/provider.tsx
@@ -4,7 +4,19 @@ import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createApolloClient } from '@/lib/apollo-client';
+import { setContext } from '@apollo/client/link/context';
 import theme from './theme';
+
+const authLink = setContext((_, { headers }) => {
+    // Get the token from localStorage or cookies
+    const token = localStorage.getItem('authToken');
+    return {
+        headers: {
+            ...headers,
+            Authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
 
 const client = createApolloClient();
 
